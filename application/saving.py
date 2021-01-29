7ba478465c92c76c327bff573840c2fbd44faf2d
Stefan Kuppelwieser <entwicklung@kuppelwieser.net>
2021-01-29 16:36:42 +0100

    Update

--- application/saving.py
+++ application/saving.py
@@ -3,6 +3,8 @@ import time
 from ogame import OGame
 from loguru import logger
 
+from ogame.constants import coordinates, status, ships, mission
+
 
 class Saving(object):
     def __init__(self, properties, empire, telegram, utils):
@@ -11,49 +13,111 @@ class Saving(object):
         self.telegram = telegram
         self.utils = utils
 
+    def wait_for_next_run(self):
+        logger.info('Check if you get attacked or not. Next run in {0} seconds.'.format(self.properties.SAVING_RECHECK_ATTACKS))
+        time.sleep(self.properties.SAVING_RECHECK_ATTACKS)
+
+    def has_planet_moon(self, target):
+        planets_scanned = []
+        for current_planet in self.empire.planet_ids():
+            cords = self.empire.celestial_coordinates(current_planet)
+            planets_scanned.extend(tmp_planets_scanned for tmp_planets_scanned in self.empire.galaxy(coordinates(cords[0], cords[1])) if tmp_planets_scanned.position == cords and tmp_planets_scanned.moon is True)
+
+        for planet_scanned in planets_scanned:
+            if planet_scanned.position == target:
+                return planet_scanned.moon
+        return False
+
+    def has_enough_deuterium(self, source_planet):
+        if self.empire.resources(source_planet).deuterium >= 10000:
+            return True
+        message = 'Not enough deuterium on planet {0} {1} to safe fleets or ressources'.format(
+            source_planet,
+            self.empire.name_by_moon_planet_id(source_planet)
+        )
+        logger.info(message)
+        self.telegram.send_message(message)
+        return False
+
     def auto_run_saving(self):
 
         enemies_cache = []
 
         while True:
-            logger.info('Check if you get attacked or not')
-
             # check if you get attacked
             if self.empire.attacked():
 
-                # check cache can be reduced
+                # Reduce list
+                for enemie_cache in enemies_cache:
+                    diff = self.utils.get_diff_minutes(enemie_cache.arrival)
+                    if diff < int(self.properties.SAVING_RECHECK_ATTACKS / 60):
+                        enemies_cache.remove(enemie_cache)
 
-                    # gehe cache durch
-                    # berechne differenz
-                    # wenn die differenz kleiner gleich 0 ist -> löschen
-                    # wenn die differenz größer 0 ist nichts machen: pass
+                # Add attack to cache list
+                new_attacks = []
+                for current_attack in self.empire.hostile_fleet():
+                    is_in_list = False
+                    for old_attack in enemies_cache:
+                        if old_attack.arrival == current_attack.arrival:
+                            is_in_list = True
+                    if is_in_list is False:
+                        new_attacks.append(current_attack)
 
-                    #diff_minutes = self.utils.get_diff_minutes(self.empire.hostile_fleet()[0].arrival)
-                    #print(diff_minutes)
+                # send message of new attacks
+                for new_attack in new_attacks:
+                    message = '!!! You are under attack from {0}, {2} at planet {3} {4}. He arrives at {5} !!!'.format(
+                        new_attack.player_name,
+                        new_attack.player_id,
+                        new_attack.origin,
+                        new_attack.destination,
+                        self.empire.name_by_moon_planet_id(
+                            self.empire.id_by_planet_moon_cords(new_attack.destination)),
+                        new_attack.arrival
+                    )
+                    logger.warning(message)
+                    self.telegram.send_message(message)
 
+                # # send probe
+                # if self.empire.send_fleet(mission.spy, self.empire.id_by_planet_moon_cords(new_attack.destination), new_attack.origin, [ships.espionage_probe(self.properties.SAVING_SEND_PROBES)]):
+                #     # get information about last spy
+                #     fleet = reversed(self.empire.friendly_fleet()).list[1]
+                #     if fleet == mission.spy:
+                #         message = 'You get attack. Send {2} probes to planet {0}. The probe arrives at {1}' \
+                #                   'list'.format(
+                #             fleet.destination,
+                #             fleet.arrival,
+                #             self.properties.SAVING_SEND_PROBES
+                #         )
+                #         logger.info(message)
+                #         self.telegram.send_message(message)
+                #         continue
 
-                # checken ob attacke eine spio ist
+                # check if planet has a moon or planet
+                planet_to_save = [3, 162, 4, 1] #Thx to PResident Neso
+                for new_attack in new_attacks:
 
-                    # angreife loppen
-                    # differenz von start und ankunft ausrechnen
-                    # ist die differenz größer 10 minuten => Angriff => cache_liste speichern => Spio mit 11 probs verschicken vom angreifenden planeten
-                    # Ansonsten ist es eine spio => telegram messagen => NICHT in cache speichern
+                    if new_attack.destination[3] == 3:
+                        # if self.has_enough_deuterium(new_attack.destination):
+                        #     pass
+                        # else:
+                        #     # TODO: get deut of mother planet
+                        #     pass
+                        #
+                        # # save from moon to planet
+                        #     # safe battle ships
+                        #     # safe ressources
+                        pass
 
-
-                    # # save enemy to the cache
-                    # if len(enemies_cache) == 0:
-                    #     enemies_cache = self.empire.hostile_fleet()
-                    # else:
-                    #     tmp_enemies_cache = enemies_cache
-                    #     enemies_cache = []
-                    #     for new in self.empire.hostile_fleet():
-                    #         exist = False
-                    #         for old in tmp_enemies_cache:
-                    #             if new.arrival == old.arrival:
-                    #                 exist = True
-                    #         if exist is False:
-                    #             enemies_cache.append(new)
-
+                    if new_attack.destination[3] == 1:
+                        has_planet_a_moon = self.has_planet_moon(new_attack.destination)
+                        #safe from planet to moon
+                            # safe battle ships
+                            # safe ressources
+                        # ELSE
+                            # safe to elnappo
+                            # safe battle ships
+                            # safe ressources
+                        pass
 
                 # save ressources
 
@@ -69,31 +133,12 @@ class Saving(object):
                     # Nachricht an Telegram raus
 
 
-                # can entfernt werden
-                # send message to enemy
-                for current_enemy in enemies_cache:
+                # add new attacks to cache
+                enemies_cache.extend(new_attacks)
 
-                    #self.empire.send_message(current_enemy.player_id, 'Seid gegrüßt {0}! {1}'.format(
-                    #                        current_enemy.player_name,
-                    #                        random.choice(self.properties.SAVING_RANDOM_TEXT)
-                    #                    ))
-
-                    # report to me
-                    message = '!!!!!!!! You are under attack from {0}, {2} at planet {3}. He arrives at {4} !!!!!!!!'.format(
-                        current_enemy.player_name,
-                        current_enemy.player_id,
-                        current_enemy.origin,
-                        current_enemy.destination,
-                        current_enemy.arrival
-                        )
-                    logger.warning(message)
-                    self.telegram.send_message(message)
-
-                # update cache
-                enemies_cache = self.empire.hostile_fleet()
             else:
                 # reset list
                 enemies_cache = []
 
-            # wait xx seconds for next check
-            time.sleep(self.properties.SAVING_RECHECK_ATTACKS)
+            # wait for next check
+            #self.wait_for_next_run()
