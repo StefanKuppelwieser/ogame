db802507129d882c47f717378da5e6585d4820c9
Stefan Kuppelwieser <entwicklung@kuppelwieser.net>
2021-03-27 08:58:47 +0100

    Update properties.py

--- application/properties.py
+++ application/properties.py
@@ -30,7 +30,7 @@ class Properties(object):
     #################################
     EXPEDITIONS_RECHECK = 120
     EXPEDITIONS_DURATION = 1
-    EXPEDITIONS_BONUS = 4 # +3 class bonus +1 admiral +0 Inventarbonus
+    EXPEDITIONS_BONUS = 6 # +3 class bonus +1 admiral +2 Inventarbonus
     EXPEDITIONS_ONLY_LARGE_CARGOS = True
     EXPEDITIONS_LARGE_CARGOS = 100
     EXPEDITIONS_SMALL_CARGOS = 200
